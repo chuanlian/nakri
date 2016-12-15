38d8c784b73dca679ae5c75710136af37ce3d3d1
yangchuanlian <yangchuanlian@baidu.com>
2016-12-15 11:08:30 +0800

    修改配置

--- src/main/java/com/baidu/rigel/hackathon17/service/impl/DuServiceImpl.java
+++ src/main/java/com/baidu/rigel/hackathon17/service/impl/DuServiceImpl.java
@@ -6,13 +6,18 @@ package com.baidu.rigel.hackathon17.service.impl;
 import com.baidu.rigel.hackathon17.dao.DuDao;
 import com.baidu.rigel.hackathon17.service.DuService;
 import com.baidu.rigel.hackathon17.utils.PropertyUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Properties;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yangchuanlian on 2016/12/14.
@@ -35,9 +40,36 @@ public class DuServiceImpl implements DuService {
      * @return
      */
     public String askQuestions(String question) {
-        Assert.notNull(question, "请输入问题");
+        if (StringUtils.isBlank(question)) {
+            throw new IllegalArgumentException("请输入问题");
+        }
+
+        List<String> words = segmentText(question);
         String answer = PropertyUtil.getProperty(question);
         return answer;
+    }
+
+
+    private List<String> segmentText(String question) {
+        List<String> words = new ArrayList<String>();
+        try {
+            //创建分词对象
+            Analyzer anal = new IKAnalyzer(true);
+            StringReader reader = new StringReader(question);
+            //分词
+            TokenStream ts = anal.tokenStream("", reader);
+            CharTermAttribute term = ts.getAttribute(CharTermAttribute.class);
+            //遍历分词数据
+            while (ts.incrementToken()) {
+                System.out.print(term.toString() + "|");
+                words.add(term.toString());
+            }
+            reader.close();
+            System.out.println();
+        } catch (Exception ex) {
+
+        }
+        return words;
     }
 
     public static void main(String[] args) {
