9a2ae6e79ea0c8d30bc0c190146216f1362f07d7
yangchuanlian <yangchuanlian@baidu.com>
2016-12-16 14:10:59 +0800

    分词查询逻辑处理

--- src/main/java/com/baidu/rigel/hackathon17/service/impl/DuServiceImpl.java
+++ src/main/java/com/baidu/rigel/hackathon17/service/impl/DuServiceImpl.java
@@ -5,19 +5,21 @@ package com.baidu.rigel.hackathon17.service.impl;
 
 import com.baidu.rigel.hackathon17.dao.DuDao;
 import com.baidu.rigel.hackathon17.service.DuService;
-import com.baidu.rigel.hackathon17.utils.PropertyUtil;
+import com.baidu.rigel.hackathon17.vo.AnswerVo;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import javax.annotation.Resource;
 import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
+import static com.baidu.rigel.hackathon17.utils.KnowledgeUtil.knowledgeMap;
 
 /**
  * Created by yangchuanlian on 2016/12/14.
@@ -27,11 +29,6 @@ public class DuServiceImpl implements DuService {
 
     @Resource(name = "duDao")
     private DuDao duDao;
-
-
-    public String sayHi(String name) {
-        return duDao.sayHi(name);
-    }
 
     /**
      * 根据问题返回答案
@@ -43,12 +40,76 @@ public class DuServiceImpl implements DuService {
         if (StringUtils.isBlank(question)) {
             throw new IllegalArgumentException("请输入问题");
         }
-
+        // 精确匹配
+        AnswerVo accurateAnswerVo = accurateMatching(question);
+        if (accurateAnswerVo != null && StringUtils.isNotBlank(accurateAnswerVo.getAnswer())) {
+            return accurateAnswerVo.getAnswer();
+        }
+        // 分词后匹配
+        List<AnswerVo> answerVoList = new ArrayList<AnswerVo>();
         List<String> words = segmentText(question);
-        String answer = PropertyUtil.getProperty(question);
-        return answer;
+        for (String word : words) {
+            matching(word, answerVoList);
+        }
+        AnswerVo maxAnswerVo = new AnswerVo(0);
+        for (AnswerVo answerVo : answerVoList) {
+            if (answerVo.getCount().intValue() > maxAnswerVo.getCount().intValue()) {
+                maxAnswerVo = answerVo;
+            }
+        }
+        return maxAnswerVo.getAnswer();
     }
 
+    /**
+     * 答案全字匹配
+     *
+     * @param word
+     * @return
+     */
+    private AnswerVo accurateMatching(String word) {
+        AnswerVo answerVo = new AnswerVo();
+        Iterator it = knowledgeMap.keySet().iterator();
+        while (it.hasNext()) {
+            String key = (String) it.next();
+            if (key.contains(word)) {
+                String answer = knowledgeMap.get(key);
+                answerVo.setKey(key);
+                answerVo.setAnswer(answer);
+            }
+        }
+        return answerVo;
+    }
+
+
+    /**
+     * 答案匹配
+     *
+     * @param answerVoList
+     * @return
+     */
+    private void matching(String word, List<AnswerVo> answerVoList) {
+        Iterator it = knowledgeMap.keySet().iterator();
+        while (it.hasNext()) {
+            String key = (String) it.next();
+            if (key.contains(word)) {
+                // 判断集合中是否包含这个答案
+                boolean flag = false;
+                for (AnswerVo answerVo : answerVoList) {
+                    if (answerVo.getKey().contains(key)) {
+                        flag = true;
+                        answerVo.setCount(answerVo.getCount() + 1);
+                        break;
+                    }
+                }
+                if (!flag) {
+                    AnswerVo answerVo = new AnswerVo();
+                    answerVo.setKey(key);
+                    answerVo.setAnswer(knowledgeMap.get(key));
+                    answerVoList.add(answerVo);
+                }
+            }
+        }
+    }
 
     private List<String> segmentText(String question) {
         List<String> words = new ArrayList<String>();
@@ -72,11 +133,7 @@ public class DuServiceImpl implements DuService {
         return words;
     }
 
-    public static void main(String[] args) {
-        try {
-            System.out.println(PropertyUtil.getProperty("1"));
-        } catch (Exception ex) {
-
-        }
+    public String sayHi(String name) {
+        return duDao.sayHi(name);
     }
 }
--- src/main/java/com/baidu/rigel/hackathon17/utils/KnowledgeUtil.java
+++ src/main/java/com/baidu/rigel/hackathon17/utils/KnowledgeUtil.java
@@ -3,12 +3,6 @@
  */
 package com.baidu.rigel.hackathon17.utils;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.wltea.analyzer.lucene.IKAnalyzer;
-
-import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,26 +14,12 @@ public class KnowledgeUtil {
     public static Map<String, String> knowledgeMap = new HashMap<String, String>();
 
     static {
-        knowledgeMap.put("asdf", "asdfads");
+        knowledgeMap.put("百度推广是什么", "百度推广是国内首创的一种按效果付费的网络推广方式，简单便捷的网页操作即可给企业带来大量潜在客户，有效提升企业知名度及销售额。每天有超过1亿人次在百度查找信息，企业在百度注册与产品相关的关键词后，就会被主动查找这些产品的潜在客户找到");
+        knowledgeMap.put("百度推广怎么收费", "我们百度推广是按有效点击来付费的，只有有效的点击系统才会扣费。有效的点击是指：系统会通过各种参数设定来判断什么样的点击会称为有效点击，具体情况可以给您安排推广顾问联系。");
+        knowledgeMap.put("百度是竞价排名么", "您的排名是由出价和质量度共同决定的，当中涉及很多因素，并不只是出价");
+        knowledgeMap.put("怎么预防恶意点击", "我们百度有非常完善的过滤系统，当中涉及十几个参数，所以您不需要担心恶意点击问题");
+        knowledgeMap.put("百度推广可以试用么", "我们为您在网站上展示信息是不收费的，只有当网民有效点击进入了您的网站，我们才会收取一定的费用");
+        knowledgeMap.put("为什么不同地域收费不一样", "“专业服务费”是指用于购买百度提供的百度推广服务专业服务的费用，但由于各地代理商会提供多元化的服务，因此会有所不同");
     }
 
-    public static void main(String[] args) {
-        try {
-            String text = "如何做百度推广";
-            //创建分词对象
-            Analyzer anal = new IKAnalyzer(true);
-            StringReader reader = new StringReader(text);
-            //分词
-            TokenStream ts = anal.tokenStream("", reader);
-            CharTermAttribute term = ts.getAttribute(CharTermAttribute.class);
-            //遍历分词数据
-            while (ts.incrementToken()) {
-                System.out.print(term.toString() + "|");
-            }
-            reader.close();
-            System.out.println();
-        } catch (Exception ex) {
-
-        }
-    }
 }
